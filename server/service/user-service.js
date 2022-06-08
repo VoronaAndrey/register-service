bef659032f457eea0263e3c214c15e404520a01d
Andrey Vorona <voronaandrey95@gmail.com>
2022-06-09 02:19:04 +0300

    deploy

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,7 +1,5 @@
 const UserModel = require('../models/user-model');
 const bcrypt = require('bcrypt');
-// const uuid = require('uuid');
-// const mailService = require('./mail-service');
 const tokenService = require('./token-service');
 const UserDto = require('../dtos/user-dto');
 const ApiError = require('../exceptions/api-error');
@@ -43,9 +41,6 @@ class UserService {
     }
 
     async refresh(refreshToken) {
-        // if (!refreshToken) {
-        //     throw ApiError.UnauthorizedError();
-        // }
         const userData = tokenService.validateRefreshToken(refreshToken);
         const tokenFromDb = await tokenService.findToken(refreshToken);
         if (!userData || !tokenFromDb) {
